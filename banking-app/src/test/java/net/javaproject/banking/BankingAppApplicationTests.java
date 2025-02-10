8e8042faf248cbdda865ced7268c958d31826067
Sounak <sounaksaha333@gmail.com>
2025-02-10 21:34:34 +0530

    0.1 => init spring boot project with packages and entity

---
+++ banking-app/src/test/java/net/javaproject/banking/BankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaproject.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
